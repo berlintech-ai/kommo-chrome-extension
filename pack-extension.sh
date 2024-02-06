282efbf741e6fdea60feba752bb225339d739e1c
Dmitrii Malakhov <mitia2022@gmail.com>
2024-02-06 15:54:16 +0100

    Add pack-extension.sh script and update .gitignore and package.json

---
+++ pack-extension.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Bump patch version
+npm version patch
+
+# Extract version from package.json
+VERSION=$(jq -r .version package.json)
+
+# Archive ./dist folder
+zip -r "dist-$VERSION.zip" ./dist
